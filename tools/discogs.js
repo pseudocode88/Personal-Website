878739cf3869552bff52f431ccc4c52d9852aabe
Dr J <jaison.justus@go-jek.com>
2019-04-08 01:53:29 +0530

    Modify discogs.js to give random release when no param passed

--- tools/discogs.js
+++ tools/discogs.js
@@ -1,56 +1,69 @@
 let https = require('https');
 let process = require('process');
+let argv = require('minimist')(process.argv.slice(2));
 
-let releaseId = (process.argv[2]) ? process.argv[2] : false;
-let trackIds = (process.argv[3]) ? process.argv[3].split(',') : [];
+const ACTIVEPROJECT = "turn-up-the-808";
 
-if(!releaseId)  {
-    console.log("");
-    console.log("Invalid params!!!");
-    console.log("");
-    console.log("discogs release_id track_no,track_no");
-    console.log("discogs 75943 1,3,7");
-    console.log("");
+function searchDiscogs(releaseId) {
+    const httpsOptions = {
+        hostname: 'api.discogs.com',
+        path: '/releases/' + releaseId + '?key=YNoLiWCOwTgGWeKngZzN&secret=vVjhSQXuHjpUtYXVTOxFiVIMsVltCEmN',
+        headers: { 'User-Agent': 'Mozilla/5.0' }
+    };
+
+    https.get(httpsOptions, function(res){
+        var body = '';
+        res.on('data', (chunk) => body += chunk);
+        res.on('end', () => onReleaseFound(releaseId, body));
+    }).on('error', (e) => showConnectionnErrorPrompt());
+}
+
+function onReleaseFound(releaseId, body) {
+    let response = JSON.parse(body);
+    if(response.hasOwnProperty('message'))  {
+        showReleaseNotFoundPrompt();
+        process.exit(1);
+    }
+
+    showSuccessPrompt(response.uri);
+    let releaseDetails = discogsResponseParser(response);
+
+    if(trackNos.length > 0) {
+        scaffoldTrackEmbed(releaseDetails);
+        showTrackEmbedPrompt(releaseDetails.tracks);
+    }
+
+    showReleaseDetails(releaseDetails);
     process.exit(1);
 }
 
-process.stdout.write('\033c');
-
-console.log("");
-console.log("Discogs");
-console.log("=======");
-console.log("> Search: r" + releaseId);
-console.log("> GET: https://api.discogs.com/releases/" + releaseId);
-
-const httpsOptions = {
-    hostname: 'api.discogs.com',
-    path: '/releases/' + releaseId,
-    headers: { 'User-Agent': 'Mozilla/5.0' }
-};
-
-https.get(httpsOptions, function(res){
-    var body = '';
-    res.on('data', (chunk) => body += chunk);
-    res.on('end', () => discogsAPIParser(JSON.parse(body)));
-}).on('error', (e) => console.log("Got an error: ", e));
-
-function getTrackList(response) {
-    if(trackIds.length === 0) {
-        return response.tracklist.map((x) => { return x.title; })
-    }
-
-    return trackIds.map((x) => { return response.tracklist[x - 1].title; });
-}
-
-function discogsAPIParser(response) {
-    generateTrackEmbed({
+function discogsResponseParser(response) {
+    return {
         id: response.id,
         uri: response.uri,
         album: response.title,
         artist: response.artists.map((x) => { return x.name; }),
-        genre: response.genres.concat(response.styles, [response.year]).join(" • "),
-        tracks: getTrackList(response)
-    });
+        genre: (response.hasOwnProperty('genres')) ? response.genres : [],
+        style: (response.hasOwnProperty('styles')) ? response.styles : [],
+        country: response.country,
+        year: response.year,
+        tracks: response.tracklist.map((x) => { return x.title; }),
+        imageUrl: (response.hasOwnProperty('images')) ? response.images[0].uri : false
+    };
+}
+
+function scaffoldTrackEmbed(data) {
+    let trackListingHTML = trackNos.filter( x => data.tracks[x-1] )
+        .map(x => { return data.tracks[x-1] })
+        .map((x) => { return trackSnippetTpl().replace("{name}", x) })
+        .join('');
+
+    pbcopy(trackEmbedTpl()
+        .replace("{title}", data.album)
+        .replace("{artist}", data.artist)
+        .replace("{genre}", data.genre.concat(data.style, [data.year]).join(" • "))
+        .replace("{tracks}", trackListingHTML));
+
 }
 
 function pbcopy(data) {
@@ -58,39 +71,85 @@ function pbcopy(data) {
     proc.stdin.write(data); proc.stdin.end();
 }
 
-function generateTrackEmbed(data)   {
-    let trackDetails = data.tracks.map((x) => { return trackSnippet.replace("{name}", x) });
-
-    pbcopy(trackEmbedSnippet
-        .replace("{title}", data.album)
-        .replace("{artist}", data.artist)
-        .replace("{genre}", data.genre)
-        .replace("{tracks}", trackDetails.join('')));
-
-    console.log("> Success: " + data.uri);
-    console.log("> TrackEmbed: Copied");
-    console.log("> Done");
+function showHeader() {
     console.log("");
-    console.log(data.album);
-    console.log("by " + data.artist);
-    console.log(data.genre);
+    console.log("Discogs");
+    console.log("=======");
+}
+
+function showSearchingPrompt(releaseId)  {
+    console.log("Search: r" + releaseId);
+    console.log("GET: https://api.discogs.com/releases/" + releaseId);
+}
+
+function showSuccessPrompt(url) {
+    console.log("Success: " + url);
+}
+
+function showTrackEmbedPrompt(tracks) {
+    let trackfound = trackNos.filter(x => tracks[x - 1]);
+    if(trackfound.length > 0) {
+        console.log("TrackEmbed: [" + trackfound.join(',') + "] Copied to clipboard");
+    }else   {
+        console.log("TrackEmbed: Invalid track numbers");
+    }
+}
+
+function showReleaseDetails(data) {
     console.log("");
-    data.tracks.forEach((x, i) => {
-        console.log(((!trackIds.length) ? i+1 : trackIds[i]) + ". " + x);
-    });
+    console.log("Album: " + data.album);
+    console.log("Artist: " + data.artist);
+    console.log("Genre: " + data.genre.join(','));
+    console.log("Style: " + data.style.join(','));
+    console.log("Country: " + ((data.country) ? data.country : ''));
+    console.log("Year: " + ((data.year) ? data.year : ''));
+    console.log("");
+    data.tracks.forEach((x, i) => { console.log((i+1) + ". " + x) });
     console.log("");
 }
 
-let trackEmbedSnippet = "<li class=\"TrackEmbed\">\n" +
-    "   <div class=\"TrackEmbed__Details\">\n" +
+function showReleaseNotFoundPrompt() {
+    console.log("Failed: Release Not Found");
+    console.log("");
+}
+
+function showConnectionnErrorPrompt() {
+    console.log("Error: No Internnet Connection");
+    console.log("");
+}
+
+function trackEmbedTpl() {
+    return (
+        "<li class=\"TrackEmbed\">\n" +
+        "   <div class=\"TrackEmbed__Details\">\n" +
         "   <img class=\"TrackEmbed__AlbumArt\" src=\"/images/stations/\"/>\n" +
         "   <div class=\"TrackEmbed__Meta\">\n" +
-            "   <h4 class=\"TrackEmbed__AlbumName\">{title}</h4>\n" +
-            "   <p class=\"TrackEmbed__ArtistName\">by {artist}</p>\n" +
-            "   <p class=\"TrackEmbed__Style\">{genre} </p>\n" +
+        "   <h4 class=\"TrackEmbed__AlbumName\">{title}</h4>\n" +
+        "   <p class=\"TrackEmbed__ArtistName\">by {artist}</p>\n" +
+        "   <p class=\"TrackEmbed__Style\">{genre} </p>\n" +
         "   </div>\n" +
-    "   </div>\n" +
-    "   <ol class=\"TrackEmbed__Tracks\">\n{tracks}    </ol>\n" +
-"</li>";
+        "   </div>\n" +
+        "   <ol class=\"TrackEmbed__Tracks\">\n{tracks}    </ol>\n" +
+        "</li>"
+    );
+}
 
-let trackSnippet = "\t<li><p>{name}</p><i class=\"Play\" data-track-id=\"\">PLAY</i></li>\n";
+function trackSnippetTpl()  {
+    return (
+        "\t<li><p>{name}</p><i class=\"Play\" data-track-id=\"\">PLAY</i></li>\n"
+    );
+}
+
+function getRandomReleaseId()   {
+    return Math.floor(Math.random() * 11001697) + 1
+}
+
+process.stdout.write('\033c');
+showHeader();
+
+let releaseId = (argv._.length <= 0) ? getRandomReleaseId() : argv._[0];
+let trackNos = (argv.hasOwnProperty('t')) ? argv.t.toString().split(',') : [];
+let downloadImage = (argv.hasOwnProperty('i')) ? true : false;
+
+showSearchingPrompt(releaseId);
+searchDiscogs(releaseId);
