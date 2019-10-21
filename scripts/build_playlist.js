489622ec2db62dd4a62d2596d78e3b7d93f4d1b3
Dr J <jaison.justus@go-jek.com>
2019-10-21 21:03:13 +0530

    Add all playlist for turn up the 808

--- scripts/build_playlist.js
+++ scripts/build_playlist.js
@@ -7,6 +7,15 @@ const playlistConfig = {
 };
 
 const playlists = [
+    'turn-up-the-808/prelude',
+    'turn-up-the-808/electro',
+    'turn-up-the-808/freestyle',
+    'turn-up-the-808/miami-bass',
+    'turn-up-the-808/favela-funk',
+    'turn-up-the-808/chicago-house',
+    'turn-up-the-808/detroit-techno',
+    'turn-up-the-808/italo-disco',
+    'turn-up-the-808/where-else',
     'sheilachandra',
     'thesaigonkick',
     'rossereysothea',
